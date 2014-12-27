f06e222daa92910c7d70d548198f798750958bb9
dahnny012 <filler012@gmail.com>
2014-12-27 04:19:06 +0000

    added project log

---
+++ ProjChaikaTODO.c
@@ -0,0 +1,21 @@
+#Last Word Smith:
+
+
+#UI:
+	- Add a history log.Weapon Queue needs to be refreshed.
+#Game Features/Bug:
+	- Option to select or maybe enforcing
+	- Either let user end input with some symbol to enforce?
+	- Let System spit out all possible results.
+#AI
+	- need a basic one.
+	- Rotate around custom made spells.
+	- player health gauge.
+
+#priority: 
+	- Finish all by newyears?
+	- Figure out selecting ---> AI ---> UI
+
+#done:
+	- fixed cases of v,v | n,n | v,adj
+	- Resolving with dropping the next Tok and doing reparsing again.
